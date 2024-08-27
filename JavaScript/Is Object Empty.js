1827cf962223b95c1550642c0a9f2c9bd3f2aea7
AbhayPc <abhaypc112@gmail.com>
2024-08-27 11:55:45 +0530

    Q10 update

--- JavaScript/Is Object Empty.js
+++ JavaScript/Is Object Empty.js
@@ -3,14 +3,19 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    return (Object.values(obj).length===0 || obj.length===0)
+    console.log(Object.keys(obj).length);
+        // if(obj!==null)
+    return (Object.keys(obj).length===0 || obj.length===0 && obj==null)
 };
 
 const obj = {"x": 5, "y": 42}
 const obj1 = {}
 const obj2 = [null, false, 0]
+const obj3={"length": 0}
 console.log(isEmpty(obj));
 console.log(isEmpty(obj1));
 console.log(isEmpty(obj2));
+console.log(isEmpty(obj3));
+
 
 
