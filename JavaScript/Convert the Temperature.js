660126dcd96f9229fa102ec85d8e951bdaaf720e
AbhayPc <abhaypc112@gmail.com>
2024-08-30 09:52:27 +0530

    Q13

---
+++ JavaScript/Convert the Temperature.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    return [(celsius+273.15),(celsius*1.80+32.00)]
+};
+const celsius=36.50;
+console.log(celsius);
