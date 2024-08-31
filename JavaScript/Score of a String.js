fbead6ce49c0c55132c3775404aa517256ac5c64
AbhayPc <abhaypc112@gmail.com>
2024-08-31 10:03:24 +0530

    Q14

---
+++ JavaScript/Score of a String.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let result=0;
+    for(let i=0;i<s.length-1;i++){
+        result+=Math.abs((s.charCodeAt(i)-s.charCodeAt(i+1)))
+    }
+    return result
+};
+
+const s = "hello";
+console.log(scoreOfString(s));
