2d17fae822cefd221628052421781d66cf6dd661
AbhayPc <abhaypc112@gmail.com>
2024-10-10 08:58:17 +0530

    Q37

---
+++ JavaScript/Reverse String.js
@@ -0,0 +1,10 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse()
+};
+
+const s = ["h","e","l","l","o"];
+console.log(reverseString(s));
