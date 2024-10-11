d1545b3df6cfc017446aad3550852eca649a9252
AbhayPc <abhaypc112@gmail.com>
2024-10-11 17:28:10 +0530

    Q38

---
+++ JavaScript/Add Digits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    if(num===0) return 0
+    return num%9===0?9:num % 9
+ };
+
+ const num = 38;
+ console.log(addDigits(num));
+ 
