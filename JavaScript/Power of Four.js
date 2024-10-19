6b58a47d16dfe32a59db0b3395f365b8fc0157fc
AbhayPc <abhaypc112@gmail.com>
2024-10-19 10:18:12 +0530

    Q35

---
+++ JavaScript/Power of Four.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n<=0) return false
+    while(n%4 === 0){
+        n/=4
+    }
+    return n === 1
+};
+const n = 16;
+console.log(isPowerOfFour(n));
