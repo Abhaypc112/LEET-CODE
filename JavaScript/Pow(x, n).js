66dfa40e0988ec8781aab13dd749c72416e1207d
AbhayPc <abhaypc112@gmail.com>
2024-10-26 15:43:41 +0530

    Q41

---
+++ JavaScript/Pow(x, n).js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x,n)
+};
+const x = 2.00000, n = 10;
+console.log(myPow(x,n));
