97d03874091c3a6350fe89c045e15787ab87a2f6
ABHAY PC <103447301+Abhaypc112@users.noreply.github.com>
2024-11-11 23:35:06 +0530

    Time: 1 ms (77.62%), Space: 51.8 MB (16.23%) - LeetHub

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let count = 0;
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] < target){
+                count++;
+            }
+        }
+    }
+     return count;
+};
