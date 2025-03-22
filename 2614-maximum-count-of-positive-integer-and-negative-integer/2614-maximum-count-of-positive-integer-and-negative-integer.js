a1c2dc500fa09287aafd192bb9d2bab28e9ee23c
ABHAY PC <abhaypc112@gmail.com>
2025-03-22 09:21:45 +0530

    Time: 1 ms (49.88%), Space: 55.3 MB (46.36%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+    let pos = 0, neg = 0;
+
+    for (let num of nums) {
+        if (num > 0) pos++;
+        else if (num < 0) neg++;
+    }
+
+    return Math.max(pos, neg);
+};
